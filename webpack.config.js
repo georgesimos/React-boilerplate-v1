dbcdb56b2c4baf21e172c02046f44e3435d41e52
George Simos <georgesimos@hotmail.gr>
2018-11-20 15:01:23 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
+    devtool: 'cheap-modeule-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    } 
+};
